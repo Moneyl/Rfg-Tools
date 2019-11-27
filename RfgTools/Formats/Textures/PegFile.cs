c3f1906c5187df158294d2779c8d8c8277218281
moneyl <8206401+Moneyl@users.noreply.github.com>
2019-11-27 12:55:48 -0500

    Change PegFile API slightly

--- RfgTools/Formats/Textures/PegFile.cs
+++ RfgTools/Formats/Textures/PegFile.cs
@@ -28,7 +28,12 @@ namespace RfgTools.Formats.Textures
         public string cpuFileName;
         public string gpuFileName;
 
-        public PegFile(string cpuFilePath, string gpuFilePath)
+        public PegFile()
+        {
+
+        }
+
+        private void SetAndCheckPaths(string cpuFilePath, string gpuFilePath)
         {
             if (!File.Exists(cpuFilePath))
             {
@@ -46,86 +51,97 @@ namespace RfgTools.Formats.Textures
             gpuFileName = Path.GetFileName(gpuFilePath);
         }
 
-        //Todo: Consider adding warnings for unusual, problematic, or unsupported values. Maybe change some existing exceptions to warnings.
-        public void Read()
+        public void Read(string cpuFilePath, string gpuFilePath)
         {
+            SetAndCheckPaths(cpuFilePath, gpuFilePath);
             using (var cpuFileStream = new FileStream(_cpuFilePath, FileMode.Open))
             {
                 using (var gpuFileStream = new FileStream(_gpuFilePath, FileMode.Open))
                 {
-                    Entries.Clear();
-                    var header = new BinaryReader(cpuFileStream);
-                    var data = new BinaryReader(gpuFileStream);
-
-                    Signature = header.ReadUInt32();
-                    if (Signature != 1447773511) //Equals GEKV as a string
-                    {
-                        throw new Exception("Header signature must be GEKV. Invalid peg file. Make sure that your packfile extractor didn't incorrectly extract the peg file you're trying to open.");
-                    }
-                    Version = header.ReadUInt16();
-                    if (Version != 10)
-                    {
-                        throw new Exception($"Unsupported peg format version detected! Only peg version 10 is supported. Version {Version} was detected");
-                    }
-
-                    Platform = header.ReadUInt16(); //Todo: Add exception or warning for unknown or unsupported platform.
-                    DirectoryBlockSize = header.ReadUInt32();
-                    if (header.BaseStream.Length != DirectoryBlockSize)
-                    {
-                        throw new Exception($"Error, the size of the header file (cpeg_pc or cvbm_pc) does not match the size value stored in the header! Actual size: {header.BaseStream.Length} bytes, stored size: {DirectoryBlockSize} bytes.");
-                    }
-
-                    DataBlockSize = header.ReadUInt32();
-                    NumberOfBitmaps = header.ReadUInt16();
-                    Flags = header.ReadUInt16();
-                    TotalEntries = header.ReadUInt16();
-                    AlignValue = header.ReadUInt16();
-
-                    //Read peg entries
-                    for (int i = 0; i < NumberOfBitmaps; i++)
-                    {
-                        var entry = new PegEntry();
-                        entry.Read(header);
-                        Entries.Add(entry);
-                    }
-
-                    //Read peg entry names
-                    foreach (var entry in Entries)
-                    {
-                        entry.Name = PegUtil.ReadNullTerminatedString(header);
-                    }
-
-
-                    //Load raw texture data from gpu file, convert to bitmaps for easy handling
-                    foreach (var entry in Entries)
-                    {
-                        entry.RawData = new byte[entry.frame_size];
-                        data.Read(entry.RawData, 0, (int)entry.frame_size);
-                        entry.Bitmap = PegUtil.EntryDataToBitmap(entry);
-                    }
+                    Read(cpuFileStream, gpuFileStream);
                 }
             }
         }
 
-        public void Write()
+        //Todo: Consider adding warnings for unusual, problematic, or unsupported values. Maybe change some existing exceptions to warnings.
+        public void Read(Stream cpuFileStream, Stream gpuFileStream)
         {
+            Entries.Clear();
+            var header = new BinaryReader(cpuFileStream);
+            var data = new BinaryReader(gpuFileStream);
+
+            Signature = header.ReadUInt32();
+            if (Signature != 1447773511) //Equals GEKV as a string
+            {
+                throw new Exception("Header signature must be GEKV. Invalid peg file. Make sure that your packfile extractor didn't incorrectly extract the peg file you're trying to open.");
+            }
+            Version = header.ReadUInt16();
+            if (Version != 10)
+            {
+                throw new Exception($"Unsupported peg format version detected! Only peg version 10 is supported. Version {Version} was detected");
+            }
+
+            Platform = header.ReadUInt16(); //Todo: Add exception or warning for unknown or unsupported platform.
+            DirectoryBlockSize = header.ReadUInt32();
+            if (header.BaseStream.Length != DirectoryBlockSize)
+            {
+                throw new Exception($"Error, the size of the header file (cpeg_pc or cvbm_pc) does not match the size value stored in the header! Actual size: {header.BaseStream.Length} bytes, stored size: {DirectoryBlockSize} bytes.");
+            }
+
+            DataBlockSize = header.ReadUInt32();
+            NumberOfBitmaps = header.ReadUInt16();
+            Flags = header.ReadUInt16();
+            TotalEntries = header.ReadUInt16();
+            AlignValue = header.ReadUInt16();
+
+            //Read peg entries
+            for (int i = 0; i < NumberOfBitmaps; i++)
+            {
+                var entry = new PegEntry();
+                entry.Read(header);
+                Entries.Add(entry);
+            }
+
+            //Read peg entry names
+            foreach (var entry in Entries)
+            {
+                entry.Name = PegUtil.ReadNullTerminatedString(header);
+            }
+
+            //Load raw texture data from gpu file, convert to bitmaps for easy handling
+            foreach (var entry in Entries)
+            {
+                entry.RawData = new byte[entry.frame_size];
+                data.Read(entry.RawData, 0, (int)entry.frame_size);
+                entry.Bitmap = PegUtil.EntryDataToBitmap(entry);
+            }
+        }
+
+        public void Write(string cpuFilePath, string gpuFilePath)
+        {
+            SetAndCheckPaths(cpuFilePath, gpuFilePath);
             using (var cpuFileStream = new FileStream(_cpuFilePath, FileMode.Truncate))
             {
                 using (var gpuFileStream = new FileStream(_gpuFilePath, FileMode.Truncate))
                 {
-                    var cpuFile = new BinaryWriter(cpuFileStream);
-                    var gpuFile = new BinaryWriter(gpuFileStream);
-
-                    WriteEntryData(gpuFile); //Write entry data to gpu file first so that entry size can be counted.
-                    WriteHeader(cpuFile);
-                    WriteEntries(cpuFile);
-                    WriteEntryNames(cpuFile);
-
-                    cpuFile.Seek(8, SeekOrigin.Begin); //Seek to cpu_file and gpu_file size values in cpu_file.
-                    cpuFile.Write((uint)cpuFile.BaseStream.Length); //Update cpu_file size variable
-                    cpuFile.Write((uint)gpuFile.BaseStream.Length); //Update gpu_file size variable
+                    Write(cpuFileStream, gpuFileStream);
                 }
             }
+        }
+
+        public void Write(Stream cpuFileStream, Stream gpuFileStream)
+        {
+            var cpuFile = new BinaryWriter(cpuFileStream);
+            var gpuFile = new BinaryWriter(gpuFileStream);
+
+            WriteEntryData(gpuFile); //Write entry data to gpu file first so that entry size can be counted.
+            WriteHeader(cpuFile);
+            WriteEntries(cpuFile);
+            WriteEntryNames(cpuFile);
+
+            cpuFile.Seek(8, SeekOrigin.Begin); //Seek to cpu_file and gpu_file size values in cpu_file.
+            cpuFile.Write((uint)cpuFile.BaseStream.Length); //Update cpu_file size variable
+            cpuFile.Write((uint)gpuFile.BaseStream.Length); //Update gpu_file size variable
         }
 
         private void WriteHeader(BinaryWriter header)
